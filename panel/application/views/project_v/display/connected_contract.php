8a51cc747f86b09cef540418a3dca2c7dd22814f
metinariturk <metinariturk@gmail.com>
2023-11-18 19:38:56 +0300

    proje özet sayfası devam

--- panel/application/views/project_v/display/connected_contract.php
+++ panel/application/views/project_v/display/connected_contract.php
@@ -10,14 +10,12 @@
         <th class="w20c">Alt Sözleşme Ekle</th>
         </thead>
         <tbody>
-        <?php foreach ($contracts
-
-                       as $contract) { ?>
+        <?php foreach ($contracts as $contract) { ?>
             <?php if ($contract->parent == 0 or $contract->parent = null) { ?>
+
                 <?php $payment_A = $this->Payment_model->sum_all(array("contract_id" => $contract->id), "A"); ?>
                 <?php $payment_B = $this->Payment_model->sum_all(array("contract_id" => $contract->id), "B"); ?>
                 <?php $contract_price = $contract->sozlesme_bedel; ?>
-
                 $this->Payment_model->sum_all(array("contract_id" => $contract->id), "A"); ?>
                 <tr>
                     <td style="text-align: center">
@@ -35,19 +33,25 @@
                     <td style="text-align: right">+ <?php echo money_format($payment_B); ?></td>
                     <td style="text-align: right">%
                         <?php if ($contract_price != 0 or $contract_price != null) { ?>
-                            <?php echo $payment_A / $contract->sozlesme_bedel * 100; ?>
+                            <?php echo floatval($payment_A / $contract->sozlesme_bedel * 100); ?>
                         <?php } ?>
                     </td>
                     <td style="text-align: center">
-                        <a href="<?php echo base_url("contract/new_form_sub/$contract->id"); ?>"><i
-                                    style="color: darkgreen" class="fa fa-plus-circle fa-lg"></i></a>
+                        <a href="<?php echo base_url("contract/new_form_sub/$contract->id"); ?>">
+                            <i style="color: darkgreen" class="fa fa-plus-circle fa-lg"></i>
+                        </a>
                     </td>
                 </tr>
                 <?php $sub_contracts = $this->Contract_model->get_all(array('parent' => $contract->id)); ?>
+                <?php $total_sub_payment_A = 0; ?>
+                <?php $total_sub_payment_B = 0; ?>
+
                 <?php foreach ($sub_contracts as $sub_contract) { ?>
                     <?php $payment_sub_A = $this->Payment_model->sum_all(array("contract_id" => $sub_contract->id), "A"); ?>
                     <?php $payment_sub_B = $this->Payment_model->sum_all(array("contract_id" => $sub_contract->id), "B"); ?>
                     <?php $sub_contract_price = $sub_contract->sozlesme_bedel; ?>
+                    <?php $total_sub_payment_A += $payment_sub_A; ?>
+                    <?php $total_sub_payment_B += $payment_sub_B; ?>
                     <tr>
                         <td style="text-align: center">
                             <a href="<?php echo base_url("contract/file_form/$sub_contract->id"); ?>">
@@ -57,9 +61,9 @@
                         <td><a href="<?php echo base_url("contract/file_form/$sub_contract->id"); ?>">
                                 <?php echo $sub_contract->sozlesme_ad; ?>
                             </a></td>
-                        <td style="text-align: right">+ <?php echo money_format($sub_contract->sozlesme_bedel); ?></td>
-                        <td style="text-align: right">+ <?php echo money_format($payment_sub_A); ?></td>
-                        <td style="text-align: right">+ <?php echo money_format($payment_sub_B); ?></td>
+                        <td style="text-align: right">- <?php echo money_format($sub_contract->sozlesme_bedel); ?></td>
+                        <td style="text-align: right">- <?php echo money_format($payment_sub_A); ?></td>
+                        <td style="text-align: right">- <?php echo money_format($payment_sub_B); ?></td>
                         <td style="text-align: right">%
                             <?php if ($contract_price != 0 or $contract_price != null) { ?>
                                 <?php echo $payment_sub_A / $sub_contract->sozlesme_bedel * 100; ?>
@@ -70,7 +74,10 @@
                     </tr>
                 <?php } ?>
             <?php } ?>
+            <?php echo $payment_A-$total_sub_payment_A; ?>
+            <?php echo $payment_B-$total_sub_payment_B; ?>
         <?php } ?>
+
         </tbody>
     </table>
 </div>
