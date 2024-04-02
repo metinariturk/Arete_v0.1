88243fd42605f323bdf8d7d0aa9fae53a51c5736
metinariturk <metinariturk@gmail.com>
2024-04-02 22:06:13 +0300

    yükleme sayfası değiştirildi

--- panel/application/views/contract_module/payment_v/common/file_list_v.php
+++ panel/application/views/contract_module/payment_v/common/file_list_v.php
@@ -1,39 +1,46 @@
-<div class="card-body file-manager image_list_container">
+<tr class="card-body file-manager image_list_container">
     <h4 class="mb-3">Evraklar
         <a href="<?php echo base_url("$this->Module_Name/download_all/$item->id"); ?>">
             <i class="fa fa-download f-18"></i>
         </a>
     </h4>
-    <ul class="files ">
+    <td class="row">
         <?php foreach ($item_files as $file) { ?>
-            <li class="file-box">
-                <div class="file-top">
-                    <?php echo ext_img($file->img_url); ?>
-                    <a href="<?php echo base_url("$this->Module_Name/file_download/$file->id"); ?>">
-                        <i class="fa fa-download f-14 ellips"></i>
-                    </a>
-                </div>
-                <div class="file-bottom">
-                    <h6><?php echo filenamedisplay($file->img_url); ?></h6>
-                    <p class="mb-1"><?php echo file_size($file->size) ?></p>
-                    <a onclick="deleteConfirmationFile(this)"
-                       url="<?php echo base_url("$this->Module_Name/fileDelete/$file->id"); ?>">
-                    <i style="font-size: 18px; color: Tomato;" class="fa fa-times-circle-o"
-                       aria-hidden="true"></i>
-                    </a>
-                </div>
-            </li>
+
+            <table class="table table-bordered">
+                <tr>
+                    <td style="width: 20px">
+                        <a href="<?php echo base_url("$this->Module_Name/file_download/$file->id"); ?>">
+                            <i class="fa fa-download"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="<?php echo base_url("$this->Module_Name/file_download/$file->id"); ?>">
+                            <?php echo ext_img($file->img_url); ?>
+                            <?php echo yazim_duzeni($file->img_url); ?> / <?php echo file_size($file->size) ?>
+                        </a>
+                    </td>
+                    <td class="col-1"><a onclick="deleteConfirmationFile(this)"
+                                         url="<?php echo base_url("$this->Module_Name/fileDelete/$file->id"); ?>">
+                            <i style="font-size: 18px; color: Tomato;" class="fa fa-times-circle-o"
+                               aria-hidden="true"></i>
+                        </a>
+                    </td>
+                </tr>
+            </table>
         <?php } ?>
-    </ul>
-    <div class="col">
-        <div class="text-end">
-            <a onclick="deleteConfirmationFile(this)"
-               class="btn btn-danger me-3" href="#"
-               data-bs-original-title=""
-               title=""
-               url="<?php echo base_url("$this->Module_Name/fileDelete_all/$item->id"); ?>"
-            ><i class="fa fa-trash-o""></i> Tümünü Sil</a>
+
         </div>
-    </div>
-</div>
+
+        <div class="col">
+            <div class="text-end">
+                <a onclick="deleteConfirmationFile(this)"
+                   class="btn btn-danger me-3" href="#"
+                   data-bs-original-title=""
+                   title=""
+                   url="<?php echo base_url("$this->Module_Name/fileDelete_all/$item->id"); ?>"
+                ><i class="fa fa-trash-o""></i> Tümünü Sil</a>
+            </div>
+        </div>
+        </div>
 
