e658bc906a46b2e89ef545571941d107cfb5a151
metinariturk <metinariturk@gmail.com>
2024-08-06 01:06:41 +0300

    little bug fix

--- panel/application/views/site_module/report_v/common/add_document.php
+++ panel/application/views/site_module/report_v/common/add_document.php
@@ -2,22 +2,30 @@
     <div class="fileuploader fileuploader-theme-dragdrop">
         <form method="post" enctype="multipart/form-data">
             <?php
-            $uploadDir = $path;
-            $preloadedFiles = array();
-            $uploadsFiles = array_diff(scandir($uploadDir), array('.', '..'));
-            foreach ($uploadsFiles as $file) {
-                if (is_dir($uploadDir . $file))
-                    continue;
-                $preloadedFiles[] = array(
-                    "name" => $file,
-                    "auc_id" => $item->id,
-                    "type" => FileUploader::mime_content_type($uploadDir . $file),
-                    "size" => filesize($uploadDir . $file),
-                    "file" => base_url($path) . $file,
-                    "local" => base_url($path) . $file,
-                );
+            $uploadDir = $path;  // Dizin yolu
+            $preloadedFiles = array();  // Önceden yüklenmiş dosyaları tutacak dizi
+
+            // Dosya yolu var mı kontrol et
+            if (!empty($uploadDir) && is_dir($uploadDir)) {
+                $uploadsFiles = array_diff(scandir($uploadDir), array('.', '..'));  // Dizin içeriğini tarar
+
+                foreach ($uploadsFiles as $file) {
+                    if (is_dir($uploadDir . $file))
+                        continue;  // Klasörleri atla
+
+                    $preloadedFiles[] = array(
+                        "name" => $file,
+                        "auc_id" => $item->id,
+                        "type" => FileUploader::mime_content_type($uploadDir . $file),
+                        "size" => filesize($uploadDir . $file),
+                        "file" => base_url($path) . $file,
+                        "local" => base_url($path) . $file,
+                    );
+                }
             }
-            $preloadedFiles = json_encode($preloadedFiles);
+
+            // Preloaded files dizisini JSON formatına çevir
+            $preloadedFiles = !empty($preloadedFiles) ? json_encode($preloadedFiles) : '[]';
             ?>
             <input type="file" name="files" data-fileuploader-files='<?php echo $preloadedFiles; ?>'>
         </form>
